0873c98e6fa3b8917acecc9c61dab27aca788fbd
Constantine Golub <constantinegolub@gmail.com>
2015-11-05 18:49:20 +0200

    Load events from API

--- app/js/app/events.js
+++ app/js/app/events.js
@@ -2,24 +2,12 @@
 define(['knockout', 'ui_strings', 'http', 'connectAPI', 'app/model'], 
 	function(ko, uiStrings, http, connectAPI, model) {
 
-	// Pagination
-	// if there are more then one page in obrained object 
-	function Pagination(JSONobj) {
-		if (!JSONobj) return;
-
-		this.currentPage = JSONobj.pagination.page_number;
-		this.totalPages = JSONobj.pagination.page_count;
-	}
-
-
 	// Events
 	function Events() {
 		this.placeholderText = uiStrings.placeholderText;
 		this.searchBtnText = uiStrings.searchBtnText;
-
 		this.searchBtnEnable = ko.observable(false);
 		this.cityName = ko.observable('');
-
 		this.isBtnEnabled = ko.computed(function() {
 			if (this.cityName() === '') {
 				this.searchBtnEnable(false);
@@ -28,18 +16,17 @@ define(['knockout', 'ui_strings', 'http', 'connectAPI', 'app/model'],
 			}
 		}, this);
 
-		this.findEvents = function() {
+		this.events = model.content.events;
+
+		this.findAndSave = function() {
 			var that = this;
-
 			connectAPI.getEvents(that.cityName()).then(function(resolve, reject) {
-				var JSONParse = JSON.parse(resolve);
-				// populate model with obtained results
-				model.add(JSONParse);
+				var events = JSON.parse(resolve);
 
-				console.log(JSONParse); // check if we get data of events at the end 
-				console.log(model); // check if model array are populated
+				model.add(events);
+				console.log(that.events());
 			});
-		}
+		};
 	}
 
 	return Events;
--- app/js/app/model.js
+++ app/js/app/model.js
@@ -3,9 +3,13 @@ define(['knockout', 'exports'], function(ko, exports) {
 
 	// MODEL
 	var model = {
-		content: [],
-		add : function(something) {
-			this.content.push(something);
+		content: {
+			events: ko.observable(),
+			pagination: ko.observable()
+		},
+		add : function(obj) {
+			this.content.events(obj.events);
+			this.content.pagination(obj.pagination);
 		}
 	};
 
